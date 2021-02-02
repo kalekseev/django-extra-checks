1dd43f6a60e42cbd27905c0f5e3758c9298aa6b5
Konstantin Alekseev <mail@kalekseev.com>
2021-02-02 14:39:18 +0300

    Fixes

--- src/extra_checks/ast/__init__.py
+++ src/extra_checks/ast/__init__.py
@@ -3,11 +3,20 @@ from typing import Type, Union
 from django.db import models
 
 from .ast import ModelAST
-from .exceptions import MissingASTError  # noqa
-from .protocols import ArgASTProtocol, FieldASTProtocol, ModelASTProtocol  # noqa
+from .exceptions import MissingASTError
+from .protocols import ArgASTProtocol, FieldASTProtocol, ModelASTProtocol
 
 
 def get_model_ast(
     model_cls: Type[models.Model],
 ) -> Union[ModelASTProtocol]:
     return ModelAST(model_cls)
+
+
+__all__ = [
+    "get_model_ast",
+    "MissingASTError",
+    "ArgASTProtocol",
+    "FieldASTProtocol",
+    "ModelASTProtocol",
+]
