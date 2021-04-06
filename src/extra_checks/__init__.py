8820f142f589a3110c9ea9dbbe680826007a8d48
Konstantin Alekseev <mail@kalekseev.com>
2021-04-07 01:40:03 +0300

    Remove default app config

--- src/extra_checks/__init__.py
+++ src/extra_checks/__init__.py
@@ -1,9 +1,6 @@
 from .check_id import CheckId
 from .registry import ignore_checks
 
-default_app_config = "extra_checks.apps.ExtraChecksConfig"
-
-
 __all__ = [
     "ignore_checks",
     "CheckId",
